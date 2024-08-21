3cac2ccc18895bbd1bac74de058921753754a873
Bijay Subedi <bijay.subedi@vedastudios.com>
2024-08-21 12:57:38 +0545

    fix: resolve the window is not undefined issue

--- src/app/utils/browserInfo.ts
+++ src/app/utils/browserInfo.ts
@@ -3,28 +3,38 @@ import Bowser from "bowser";
 interface BrowserInfo {
   name: string;
   version: string;
-  isDarkTheme: boolean;
+  isDarkTheme: boolean | null;
   os: string;
   platform: string;
 }
 
 export const getBrowserInfo = (): BrowserInfo => {
-  const browser = Bowser.getParser(window.navigator.userAgent);
+  if (typeof window !== "undefined") {
+    const browser = Bowser.getParser(window.navigator.userAgent);
 
-  const browserName = browser.getBrowserName(); // e.g., "Chrome", "Firefox", "Safari", etc.
-  const browserVersion = browser.getBrowserVersion(); // e.g., "91.0.4472.124"
-  const os = browser.getOSName(); // e.g., "Windows", "macOS", "Linux"
-  const platform = browser.getPlatformType(); // e.g., "desktop", "mobile", "tablet"
+    const browserName = browser.getBrowserName(); // e.g., "Chrome", "Firefox", "Safari", etc.
+    const browserVersion = browser.getBrowserVersion(); // e.g., "91.0.4472.124"
+    const os = browser.getOSName(); // e.g., "Windows", "macOS", "Linux"
+    const platform = browser.getPlatformType(); // e.g., "desktop", "mobile", "tablet"
 
-  const isDarkTheme =
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches;
+    const isDarkTheme =
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    return {
+      name: browserName.toLowerCase(),
+      version: browserVersion,
+      isDarkTheme: isDarkTheme,
+      os: os.toLowerCase(),
+      platform: platform.toLowerCase(),
+    };
+  }
 
   return {
-    name: browserName.toLowerCase(),
-    version: browserVersion,
-    isDarkTheme: isDarkTheme,
-    os: os.toLowerCase(),
-    platform: platform.toLowerCase(),
+    name: "",
+    version: "",
+    isDarkTheme: null,
+    os: "",
+    platform: "",
   };
 };
